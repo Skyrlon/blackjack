b2497c90e135e1a5f35b0871ccbd635cf8c2563b
Antoine Drouault <antoinedrouault@hotmail.com>
2023-04-27 23:48:44 +0200

    Add bet amount incrementation

--- src/Bets.jsx
+++ src/Bets.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 export default function Bets({ chosenBet }) {
   const bets = [1, 5, 10, 50, 100, 500, 1000];
 
+  const [currentBet, setCurrentBet] = useState(0);
+
+  const onClickButton = (bet) => {
+    setCurrentBet((v) => v + bet);
+  };
+
   return (
     <div>
-      {bets.map((bet) => (
-        <button key={bet} onClick={() => chosenBet(bet)}>
-          {bet}
-        </button>
-      ))}
+      <div>
+        {bets.map((bet) => (
+          <button key={bet} onClick={() => onClickButton(bet)}>
+            {bet}
+          </button>
+        ))}
+      </div>
+      <div>{currentBet}</div>
+      <button disabled={currentBet === 0} onClick={() => chosenBet(currentBet)}>
+        OK
+      </button>
     </div>
   );
 }
